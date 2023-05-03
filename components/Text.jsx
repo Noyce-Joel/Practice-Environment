f6d0d327e709c235440b07a1f2b37358f1e25136
Joel Noyce <noyce.joel@gmail.com>
2023-05-03 16:57:48 +0100

    Again

--- components/Text.jsx
+++ components/Text.jsx
@@ -6,56 +6,47 @@ const Text1 = (props) => {
   const [ref, springy] = useInView(
     () => ({
       from: {
-       
-        y:150,
+        y: 150,
       },
       to: {
-        
         y: 0,
       },
       config: { tension: 210, friction: 250, mass: 25 },
     }),
     {
       rootMargin: "0% 0%",
-      once: true
-    },
-    
+      once: true,
+    }
   );
 
   const [refH1, springier] = useInView(
     () => ({
       from: {
-       
-        x:-150,
+        x: -150,
       },
       to: {
-        
         x: 0,
       },
       config: { tension: 210, friction: 250, mass: 25 },
     }),
     {
       rootMargin: "0% 0%",
-      once: true
-    },
-    
+      once: true,
+    }
   );
 
-  
   return (
     <div>
-      <animated.div
-      ref={refH1} style={{ ...springier}}
-      className='text-h1'
-    >
-      BERLIN
-    </animated.div>
-    <animated.div
-      ref={ref} style={{ ...springy}}
-      className='text'
-    >
-     I am a massive great big gorgeous, loved up, lovely love who would love for you to keep scrolling to see more of my loveableness and amazing art borne out of my incredible lovliness. He needs me, he needs me, he needs me, he needs me, oh, how he needs me. I am also extremely cute but you must not go on about it! HeHeHe!
-    </animated.div>
+      <animated.div ref={refH1} style={{ ...springier }} className="text-h1">
+        BERLIN
+      </animated.div>
+      <animated.div ref={ref} style={{ ...springy }} className="text">
+        I am a massive great big gorgeous, loved up, lovely love who would love
+        for you to keep scrolling to see more of my loveableness and amazing art
+        borne out of my incredible lovliness. He needs me, he needs me, he needs
+        me, he needs me, oh, how he needs me. I am also extremely cute but you
+        must not go on about it! Hehehe
+      </animated.div>
     </div>
   );
 };
