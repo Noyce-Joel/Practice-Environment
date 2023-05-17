bf412db95605121e7a2daec77b8318372eca8931
Joel Noyce <noyce.joel@gmail.com>
2023-05-17 21:38:11 +0100

    New photos2

--- components/Pictures.js
+++ components/Pictures.js
@@ -37,12 +37,12 @@ function Images(){
           1 + (1 - data.range(2 / 3, 1 / 3)) / 3;
       });
       return <group ref={group}>
-        <Image rotation={[0, 0.5 / 1.7 ,0]} position={[-2, 0, 0]} scale={[4, height, 1]} url="/4.jpeg" />
-          <Image  position={[2, -1, 1]} scale={3} url="/2.jpeg" />
-          <Image position={[-1.4, -height, 3.5]} scale={[1, 3, 1]} url="/3.jpeg" />
-          <Image position={[-0.6, -7.5, 3]} scale={[1, 2, 1]} url="/1.jpeg" />
-          <Image position={[0.75, -height+0.5, 3.4]} scale={1} url="/5.jpeg" />
-          <Image position={[0, -height * 1.5, 2.5]} scale={[width, -height / 2, 1.5]} url="/6.jpeg" />
+        <Image rotation={[0, 0.5 / 1.7 ,0]} position={[-2, 0, 0]} scale={[4, height, 1]} url="/4.jpg" />
+          <Image  position={[2, -1, 1]} scale={3} url="/1.jpg" />
+          <Image position={[-1.4, -height, 3.5]} scale={[1, 3, 1]} url="/3.jpg" />
+          <Image position={[-0.6, -7.5, 3]} scale={[1, 2, 1]} url="/2.jpg" />
+          <Image position={[0.75, -height+0.5, 3.4]} scale={1} url="/5.jpg" />
+          <Image position={[0, -height * 1.5, 2.5]} scale={[width, -height / 2, 1.5]} url="/bg2.jpg" />
           <Image position={[0, -height * 2 - height / 4, 0]} scale={[width, height / 2, 1]} url="/bg.jpg" />
       </group>;
     
